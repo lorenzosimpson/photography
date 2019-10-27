17614ec93fe71234c36b6d5b8abbc3ffa11bf8f2
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-27 13:34:05 -0400

    adds submit logic and clear form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Navigation from "./components/Navbar";
 import Carousel from './components/Carousel';
 import Header from './components/Header';
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
-import { Form, Row, Col, FormGroup, Input, Jumbotron, Container } from 'reactstrap';
+import { Form, Row, Col, FormGroup, Input, Jumbotron, Container, Button } from 'reactstrap';
 
 const Contact = (props) => {
-    const [contactMessage, setContactMessage] = useState({})
+    const [contactMessage, setContactMessage] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
+
     const handleChange = e => {
-        e.preventDefault()
         setContactMessage({
             ...contactMessage,
             [e.target.name]: e.target.value
         })
         console.log(contactMessage)
+    }
+    const handleSubmit  = e => {
+        e.preventDefault()
+        console.log(contactMessage)
+    }
+
+    const clearForm = e => {
+        e.preventDefault()
+        setContactMessage({
+            name:'',
+            email: '',
+            message:''
+        })
     }
     
   return (
@@ -25,19 +42,23 @@ const Contact = (props) => {
                 <Row form>
                 <Col md={6}>
                 <FormGroup>
-                    <Input type="name" name="name" id="name" placeholder="Name" onChange={handleChange}/>
+                    <Input type="name" name="name" id="name" placeholder="Name" value={contactMessage.name} onChange={handleChange}/>
                 </FormGroup>
                 </Col>
                 <Col md={6}>
                 <FormGroup>
-                    <Input type="email" name="email" id="exampleEmail" placeholder="Email" onChange={handleChange}/>
+                    <Input type="email" name="email" id="exampleEmail" placeholder="Email" value={contactMessage.email} onChange={handleChange}/>
                 </FormGroup>
                 </Col>
             </Row>
         
             <FormGroup>
-                <Input type="textarea" name="message" id="message" placeholder='Message'onChange={handleChange}/>
+                <Input type="textarea" name="message" id="message" placeholder='Message' value={contactMessage.message} onChange={handleChange}/>
             </FormGroup>
+            
+            <Button onClick={handleSubmit} color="dark" size="lg">Send Message</Button>{' '}
+            <Button onClick={clearForm} color="secondary" size="lg">Clear Form</Button>
+
 
             </Form>
 
